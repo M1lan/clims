47b9819e42379ad9c0a7c61b0366a64e8283b252
eric <eric@Demeter>
2015-07-02 17:49:09 -0700

    database creation sql scripts work now

--- database/almanac.sql
+++ database/almanac.sql
@@ -1,9 +1,1 @@
-create table almanac as
-	select a.Date 
-	from (
-		select curdate() - INTERVAL (a.a + (10 * b.a) + (100 * c.a)) DAY as Date
-		from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
-		cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
-		cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as c
-	) a
-	where a.Date between '2015-07-07' and '2016-07-07' 
+create table almanac as with recursive dates as ( select '2015-01-01'::date as date union all select date + 1 from dates where date < '2016-12-12' ) select date from dates where date between '2015-07-07' and '2016-07-07';

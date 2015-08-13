0e71fd531d945589d544d97ca1d87a3041561ee6
Eric Clark <clarke21@students.wwu.edu>
2015-08-13 15:33:31 -0700

    added sale and order handling to queries.lisp

--- src/queries.lisp
+++ src/queries.lisp
@@ -1,5 +1,9 @@
 (in-package :clims)
 
+;;;TODO: Handle returns (cancelled sales)
+;;;TODO: Handle returned orders
+;;;TODO: allow for personal editing of tables (initial insert for example)
+
 ;;toy method for adding to suppliers
 (defprepared add-to-suppliers 
     (:insert-into 'suppliers :set 'supplier_id '$1 'name '$2))
@@ -17,7 +21,7 @@ For use in creating a new manufacturing batch"
 ;;;mat-info = (material quantity)
 ;;;material = (name id)
 
-;;TODO: Handle case where quantity would go negative
+;;;manufacturing
 (defun update-raw-material-quantity (mat-info)
   "updates a raw_material using the change list provided"
   (unless (= 0 (apply '+ (second mat-info)))
@@ -74,15 +78,14 @@ For use in creating a new manufacturing batch"
 
 ;;;postmodern/s-sql dont play nice with serials, so query to get highest number so far is required.
 ;;;look into sequences for fixing manual serials problem
-(defun create-process (batch-info)
+(defun create-manufacturing (name)
   "creates an entry in the manufacturing table"
   (let ((batch (incf (caar (query 
 			    (:limit
 			     (:order-by
 			      (:select 'batch_id :from 'manufacturing)
 			      (:desc 'batch_id))
-			     1)))))
-	(name (second batch-info)))
+			     1))))))
     (query (:insert-into 'manufacturing :set 'batch_id batch 'name name))
     batch))
 
@@ -91,16 +94,15 @@ For use in creating a new manufacturing batch"
 ;;;quantities = (amounts-used amounts-returned)
 ;;;batch-info = (batch name)
 
-(defun create-manufacturing (outputs inputs batch-info)
+(defun handle-manufacturing (outputs inputs batch-name)
   "creates a manufacturing batch and all of the associated inputs and outputs, edits raw materials"
   (handler-case
       (progn
 	(dolist (input inputs)
 	  (unless (= (second input) (apply '+ (first (third input))))
 	    (error () nil)))	
-	(with-transaction (derp) 
-	  (print derp)
-	  (let ((batch-info (cons (create-process batch-info) (cdr batch-info))))
+	(with-transaction (manufacturing) 	  
+	  (let ((batch-info (cons (create-manufacturing batch-name) (cons batch-name '()))))
 	    (dolist (output outputs)
 	      (create-finished-material (first batch-info) output))
 	    (let ((element-materials (loop
@@ -128,6 +130,92 @@ For use in creating a new manufacturing batch"
      for b in (second quantities)
      collect
        (- b a)))
-  
 
-;;TODO: handle creating an order, and a sale, with updated and createions to tables where necessary
+(defun order-view-data ()
+    "Returns a list, first element containing the suppliers the second containing the raw materials.
+For use in creating a new order"
+  (cons (query (:select '* :from 'suppliers))
+	(cons (query (:select '* :from 'raw_materials))
+	      '())))
+
+;;;order
+(defun create-order (supplier)
+  (let ((order (incf (caar (query 
+			    (:limit
+			     (:order-by
+			      (:select 'order_id :from 'orders)
+			      (:desc 'order_id))
+			     1))))))
+    (query (:insert-into 'orders :set 'order_id order 'supplier_id supplier))
+    order))
+
+(defun create-raw-material (name quantity cost order)
+  (query (:insert-into 'raw_materials :set 'name name
+		                           'quantity_bought quantity
+					   'quantity_fresh quantity
+					   'quantity_used_1 0
+					   'quantity_used_2 0
+					   'quantity_used_3 0
+					   'quantity_trashed 0
+					   'cost cost
+					   'order_id order)))
+
+;;materials = ((name, quantity, cost))
+(defun handle-order (materials supplier)
+  (handler-case
+      (with-transaction(order)
+	(let ((order (create-order supplier)))
+	  (dolist (material materials)
+	    (create-raw-material (first material) (second material) (third material) order))))
+    (error (e)
+      (print "error processing the order")
+      (print e))))
+
+(defun sale-view-data ()
+   "Returns a list, first element containing finished materials the second containing buyers.
+For use in creating a new sale"
+  (cons (query (:select '* :from 'finished_materials))
+	(cons (query (:select '* :from 'buyers))
+	      '())))
+
+;;;sale
+(defun create-sale (buyer)
+    (let ((sale (incf (caar (query 
+			    (:limit
+			     (:order-by
+			      (:select 'sale_id :from 'sales)
+			      (:desc 'sale_id))
+			     1))))))
+    (query (:insert-into 'sales :set 'sale_id sale 'buyer_id buyer))
+    sale))
+
+;;material = (name batch-id)
+(defun create-sold (material quantity price sale)
+  (query (:insert-into 'sold :set 'name (first material)
+		                  'batch_id (second material)
+				  'sale_id sale
+				  'quantity_sold quantity
+				  'price price))
+  (debit-finished-material material quantity))
+
+(defun debit-finished-material (material quantity)
+  (let ((stock (caar (query (:select 'quantity_in_stock
+				     :from 'finished_materials
+				     :where (:and (:= 'name (first material))
+						  (:= 'batch_id (second material))))))))
+    (query (:update 'finished_materials
+		    :set 'quantity_in_stock (- stock quantity)
+		    :where (:and (:= 'name (first material))
+				 (:= 'batch_id (second material)))))))
+
+;;material = (name batch-id)
+;;material-infos = ((material quantity price))
+(defun handle-sale (material-infos buyer)
+  (handler-case 
+      (with-transaction (sale)
+	(let ((sale_id (create-sale buyer)))
+	  (dolist (material-info material-infos)
+	    (create-sold (first material-info) (second material-info) (third material-info) sale_id))))
+    (error (e)
+      (print "error processing the sale")
+      (print e))))

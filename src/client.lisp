855182bd68b0c7f5f415ea37b326234e0931f6d9
Gaige Pierce-Raison <gaige@chatsubo.net>
2015-08-15 21:40:48 -0700

    * Updated HMAC crypto algorithm * Fixed issue of failing message auth in api-request

--- src/client.lisp
+++ src/client.lisp
@@ -6,9 +6,8 @@
 (defun sign-request (method json-params shared-secret)
   (log:debug json-params shared-secret)
   (let ((hmac (ironclad:make-hmac (sb-ext:string-to-octets shared-secret)
-                                  'ironclad:SHA1)))
+                                  'ironclad:SHA256)))
     (ironclad:update-hmac hmac (sb-ext:string-to-octets method))
-    (ironclad:update-hmac hmac (sb-ext:string-to-octets shared-secret))
     (ironclad:update-hmac hmac (sb-ext:string-to-octets json-params))
     (with-output-to-string (out)
       (s-base64:encode-base64-bytes (ironclad:hmac-digest hmac) out nil))))
@@ -16,15 +15,19 @@
 (defun api-request (method params)
   (log:error (json:encode-json-to-string (cdr (assoc "params" params :test 'string=))))
   (let* ((cookie-jar (make-instance 'drakma:cookie-jar))
-	 (shared-secret (drakma:http-request "http://localhost:8000/login" 
-					     :method :post
-					     :content-type "text/json"
-					     :parameters  '(("username" . "admint@test.com")
-							    ("password" . "pass"))
-					     :cookie-jar cookie-jar))
+	 (shared-secret (progn 
+			  (drakma:http-request "http://localhost:8000/login"
+					       :method :post
+					       :content-type "text/json"
+					       :parameters  '(("username" . "admint@test.com")
+							      ("password" . "pass"))
+					       :cookie-jar cookie-jar)
+			  (drakma:http-request "http://localhost:8000/key"
+					       :method :get
+					       :cookie-jar cookie-jar)))
 	 (json-params (json:encode-json-to-string params))
-	 (digest (sign-request 
-		  method 
+	 (digest (sign-request
+		  method
 		  (json:encode-json-to-string (cdr (assoc "params" params :test 'string=)) )
 		  shared-secret)))
     (log:info shared-secret digest json-params)
